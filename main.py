9cd53bc10b1f14a8e465ac9ce68b9998bedc374d
Shumpei <ch4nge.lightening@gmail.com>
2021-09-07 22:59:59 +0900

    main.pyでjpg出力を確認

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+import re
 import glob
 from src.convert_heic_to_jpeg import converter
-import logging
-import re
-
-logger = logging.getLogger(__name__)
 
 all_files = glob.glob('./pictures/input/*')
 HEIC_files = [ file for file in all_files if re.search('/*\.(HEIC|heic)', file)]
 
 
-for i in HEIC_files:
-    logger.info(i)
-    print(i)
-    converter(i)
+for file in HEIC_files:
+    converter(file)
