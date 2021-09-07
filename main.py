49e1eac135974d5e28d5d6718bef4596b85f8abd
Shumpei <ch4nge.lightening@gmail.com>
2021-09-07 22:54:51 +0900

    heicが大文字でも小文字でも読み込めるように修正

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import glob
 from src.convert_heic_to_jpeg import converter
+import logging
+import re
 
-HEIC_files = glob.glob('./pictures/input/*.HEIC')
+logger = logging.getLogger(__name__)
 
-converter()
+all_files = glob.glob('./pictures/input/*')
+HEIC_files = [ file for file in all_files if re.search('/*\.(HEIC|heic)', file)]
+
+
+for i in HEIC_files:
+    logger.info(i)
+    print(i)
+    converter(i)
